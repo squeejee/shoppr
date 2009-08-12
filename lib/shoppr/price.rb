c0ddcc21208eb5a8af76ff1ccc36e1de91bf02e8
Wynn Netherland <wynn@squeejee.com>
2009-08-11 19:47:32 -0500

    Pruned renamed files

--- lib/shoppr/price.rb
+++
@@ -1,9 +0,0 @@
-module Shoppr
-  class Price
-    include ROXML
-    xml_convention {|val| val.camelize(:lower) }
-    xml_reader :value, :from => :content, :as => Integer
-    xml_reader :currency, :from => :attr
-    xml_reader :check_site?, :from => :attr
-  end
-end
